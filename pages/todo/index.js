d2f92eac8dda45262ee61f1ab6dcf4d6d713c5fc
Rubs <113114471+rvbs1337@users.noreply.github.com>
2024-05-24 00:46:19 +0000

    finalizado

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -18,23 +18,43 @@ function addTask(event) {
   li.innerHTML = `
       <h2>${taskTitle}</h2>
       <p>${taskDescription}</p>
+      <button class="edit-btn" title="Editar tarefa" onclick="openEditDialog(${taskId})">✏️</button>
   `
 
   taskList.appendChild(li)
 
   // Salvar tarefas no localStorage
   const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
-  tasks.push({ title: taskTitle, description: taskDescription })
+  tasks.push({ id: taskId, title: taskTitle, description: taskDescription })
   localStorage.setItem(taskKey, JSON.stringify(tasks))
 
   form.reset()
 }
 
-// Carregar tarefas do localStorage ao recarregar a página
 window.addEventListener('DOMContentLoaded', () => {
   const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
   const taskList = document.querySelector('#taskList')
   taskList.innerHTML = tasks
-    .map((task) => `<li><h2>${task.title}</h2><p>${task.description}</p></li>`)
+    .map((task) => `
+            <li id="${task.id}">
+                <h2>${task.title}</h2>
+                <p>${task.description}</p>
+                <button class="edit-btn" title="Editar tarefa" onclick="openEditDialog(${task.id})">✏️</button>
+            </li>
+        `)
     .join('')
 })
+
+function openEditDialog(taskId) {
+  const tasks = JSON.parse(localStorage.getItem(taskKey))
+  const task = tasks.find(task => task.id === taskId)
+  document.querySelector('#edit_task_id').value = taskId
+  document.querySelector('#edit_title').value = task.title
+  document.querySelector('#edit_description').value = task.description
+
+  document.querySelector('#edit_dialog').showModal()
+}
+
+function closeEditDialog() {
+  document.querySelector('#edit_dialog').close()
+}
